5bf6c7e3e9eb81db62ef6dd1e204bd705e8ec82e
MorningHut <morninghut@outlook.com>
2023-10-15 17:45:54 +0800

    feat: Update whodocker.sh
    Add color to output, refactor with docker format

--- whodocker.sh
+++ whodocker.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
-# check which docker is using GPU
+#
+# =================================================================
+# INTRODUCTION:
+#
+# This script can find which Docker container the Python program
+# that is currently occupying the GPU memory belongs to.
+# =================================================================
+# DETAIL:
+#
+# The script will filter out Python from the output of nvidia-smi,
+# extract the PID, obtain the corresponding container ID
+# from /proc/PID/cgroup, and then search
+# for the corresponding container name in `docker ps`
+# using the obtained container ID, and finally output it.
+#
+# The intermediate results during the running process
+# will be temporarily saved in /tmp/t0.txt and t1.txt.
+# After the running is completed, both files will be deleted.
+# =================================================================
+
+blue="\033[1;96m"
+nocol="\033[0m"
+red="\033[1;95m"
 nvidia-smi | grep python | awk '$4 {print $5}' | awk ' !x[$0]++' >> /tmp/t0.txt
 for line in `cat /tmp/t0.txt`
 do
-        echo pid:
-        echo "  $line"
+        echo  -n -e "$blue PID:$nocol"
+        echo -e "$red $line $nocol"
         cat /proc/$line/cgroup | awk ' NR==1 ' | tr '/' ' ' | awk '$3 {print $3}' | cut -c 1-5 >> /tmp/t1.txt
         for line2 in `cat /tmp/t1.txt`
         do
-                echo docker:
-                docker ps | grep $line2 | awk '$11 {print $15}'
+                echo -n -e "$blue CONTAINER:$nocol "
+                docker ps --format '{{.ID}}\t{{.Names}}' | grep $line2 | awk -F'\t' -v red="\033[1;95m" -v nocol="\033[0m" '{print red $2 nocol}'
         done
         echo
         rm /tmp/t1.txt
-        #echo test
-        #echo $dockername
-        #echo dockername:$dockername
-        #shortname=echo $dockername | cut -c 1-5
-        #echo $shortname
-        #docker ps | grep 9253
-        # | grep $dockername
-        #echo $dockername | awk 'NR==1{print}'
 done
 rm /tmp/t0.txt
